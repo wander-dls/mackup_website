4964fdc7ce499e26e2deb563786d44a0784204e7
wander-dls <wd.delossantos@gmail.com>
2022-08-16 16:24:58 -0400

    completed website

---
+++ src/components/index.js
@@ -0,0 +1,27 @@
+import Navbar from './Navbar'
+import Billing from './Billing'
+import Business from './Business'
+import Button from './Button'
+import CardDeal from './CardDeal'
+import Client from './Client'
+import CTA from './CTA'
+import FeedbackCard from './FeedbackCard'
+import Footer from './Footer'
+import GetStarted from './GetStarted'
+import Hero from './Hero'
+import Stats from './Stats'
+import Testimonials from './Testimonials'
+
+export {
+    Navbar,
+    Billing,
+    CardDeal,
+    Business,
+    Client,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero,
+
+}
